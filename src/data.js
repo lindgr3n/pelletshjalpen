f7fb52266202545dc8114265799753f9127cbac3
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-14 22:14:38 +0100

    Added fake data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const store = {
+  date: '20171001',
+  total: 300
+};
+
+export const events = [
+  {
+    date: '20171114',
+    event: 'Påfyllt',
+    message: 25
+  },
+  {
+    date: '20171110',
+    event: 'Sotat',
+    message: ''
+  },
+  {
+    date: '20171001',
+    event: 'Beställt',
+    message: '3 pallar'
+  }
+];
