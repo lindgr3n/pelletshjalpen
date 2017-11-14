c7a367cb773c19a739da4dce661c3fa3f434e5c0
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-14 23:14:26 +0100

    Added hot reload

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+  });
+}
