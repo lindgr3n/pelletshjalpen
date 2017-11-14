388cfb9a00cdee5bc455ff85a9ea9051d928c062
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-14 21:11:58 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>logo</div>
+          <h1>Pelletshjälpen</h1>
+          <div>Profile</div>
+        </header>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
