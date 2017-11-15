6eabc5d0196c8af85b1ad62aa4ce5b61f84bbeb5
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-15 01:05:08 +0100

    Added id

--- src/data.js
+++ src/data.js
@@ -5,16 +5,19 @@ export const STORE_DATA = {
 
 export const EVENTS_DATA = [
   {
+    id: 1,
     date: '20171114',
     event: 'Påfyllt',
     message: 25
   },
   {
+    id: 2,
     date: '20171110',
     event: 'Sotat',
     message: ''
   },
   {
+    id: 3,
     date: '20171001',
     event: 'Beställt',
     message: '3 pallar'
