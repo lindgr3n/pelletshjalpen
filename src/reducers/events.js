e78f76dc9af44eb2eb56ed01d6d12c97a8cf76e5
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-16 00:39:50 +0100

    Working reducer

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,5 +1,10 @@
-export const events = (state = [], action) => {
-  console.log('The post will change');
-  console.log(state, action);
+export const events = (state = {}, action) => {
+  switch (action.type) {
+    case 'EVENT_ADD_ITEM':
+      return Object.assign({}, state, { events: [action.data, ...state.events] });
+
+    default:
+      break;
+  }
   return state;
 };
