69afc70a06f4e22dffe6bfb6d180cf7577ea9a22
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-16 21:08:57 +0100

    Fix id sets correct

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,8 +1,8 @@
 export const events = (state = {}, action) => {
   switch (action.type) {
     case 'EVENT_ADD_ITEM':
+      action.data.id = state.events.length + 1; // Add a new id
       return Object.assign({}, state, { events: [action.data, ...state.events] });
-
     default:
       break;
   }
