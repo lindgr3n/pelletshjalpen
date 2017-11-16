07023f197e90ca85400293e0ae8b54c437bdf92c
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-16 01:15:02 +0100

    Added sentry loggin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Raven from 'raven-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+Raven.config(process.env.REACT_APP_RAVEN_KEY).install();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
