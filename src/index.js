a96b3f0a904e6f3e63f00d8b71c03750758fa605
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-11-29 21:29:18 +0100

    Cleanup index

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { init as firebaseInit, addNewEvent } from './firebase';
-import { addEventItem } from './actions/actionCreator';
+// import { init as firebaseInit, addNewEvent } from './firebase';
+// import { addEventItem } from './actions/actionCreator';
 
 Raven.config(process.env.REACT_APP_RAVEN_KEY).install();
 
-firebaseInit();
+// firebaseInit();
 
-addNewEvent(1, '20171114', 'Påfyllt', 25, '');
+// addNewEvent(1, '20171114', 'Påfyllt', 25, '');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
