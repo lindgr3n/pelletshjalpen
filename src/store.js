81d26e205ba57c602a3fa458e79defe7029cf2e9
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-12-01 08:37:41 +0100

    Add redux console and cleanup

--- src/store.js
+++ src/store.js
@@ -6,34 +6,11 @@ import { reactReduxFirebase, firebaseReducer, getFirebase } from 'react-redux-fi
 // Get reducers
 import rootReducer from './reducers';
 
-// Get inital data
-import { EVENTS_DATA, STORE_DATA } from './data';
-
-const defaultState = {
-  events: EVENTS_DATA,
-  overviewdata: STORE_DATA
-};
-
-// Can test the redux by using in chrome devtools -> react when selecting Provider
-// $r.store.dispatch({type: 'EVENT_ADD_ITEM', data: {id: 5, date: '20171115', event: 'Sotat', message:''}})
-
 // react-redux-firebase config
 const rrfConfig = {
   userProfile: 'users'
   // useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
 };
-
-/* const createStoreWithFirebase = compose(
-  reactReduxFirebase(firebase, rrfConfig) // firebase instance as first argument
-  // reduxFirestore(firebase) // <- needed if using firestore
-)(createStore); */
-
-/* // Add Firebase to reducers
-const firebaseRootReducer = combineReducers({
-  firebase: firebaseReducer,
-  rootReducer
-  // firestore: firestoreReducer // <- needed if using firestore
-}); */
 
 const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : f => f);
 // const store = createStoreWithFirebase(rootReducer, defaultState, applyMiddleware(thunk));
@@ -42,9 +19,10 @@ const store = createStore(
   {},
   compose(
     applyMiddleware(
-      thunk.withExtraArgument(getFirebase) // Pass getFirebase functioin as extra argument
+      thunk.withExtraArgument(getFirebase) // Pass getFirebase function as extra argument
     ),
-    reactReduxFirebase(firebase, rrfConfig)
+    reactReduxFirebase(firebase, rrfConfig),
+    enhancers
   )
 );
 
