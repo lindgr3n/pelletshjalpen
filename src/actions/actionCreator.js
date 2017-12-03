82e4f3ca89534b433237dbe9d6a11d77e12a1286
Johan Lindgren <johan.lindgren@c4contexture.com>
2017-12-04 00:54:56 +0100

    Cleanup

--- src/actions/actionCreator.js
+++ src/actions/actionCreator.js
@@ -17,30 +17,15 @@ const addEventItemAction = data => {
 export const getEvents = data => {
   return async (dispatch, getState, getFirebase) => {
     const firebase = getFirebase();
-    const { firebaseRef } = getState();
-    console.log('test', firebaseRef);
-    // const data = await firebase.watchEvent('value', 'events');
     const fbRefdata = firebase.ref('/events');
     let data = {};
-    const dataSnapshot = await fbRefdata.once('value');
-    /* fbRefdata.once('value', snapshot => {
-      snapshot.forEach(function(child) {
-        console.log('SNAP', child.key, child.val());
-        data[child.key] = child.val();
-      });
-    }); */
+
     const snapshot = await fbRefdata.once('value');
     snapshot.forEach(function(child) {
-      console.log('SNAP', child.key, child.val());
       data[child.key] = child.val();
     });
-    console.log(fbRefdata, { data });
-    /*  fbRefdata.on('value', snapshot => {
-      dispatch({
-        type: FETCH_POSTS,
-        payload: snapshot.val()
-      });
-    }); */
+
+    // console.log(fbRefdata, { data });
     dispatch(getEventsAction(data));
   };
 };
